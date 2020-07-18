175d3f8db74e436389c8e40c6adedd97b483f896
dogballlee <zeus.1989@163.com>
2020-07-18 23:09:06 +0800

    NO ITS NOT

--- prjt_cv/use_Keras.py
+++ prjt_cv/use_Keras.py
@@ -1,8 +1,22 @@
-from tensorflow import keras
-from tensorflow.data import Dataset
-import pandas as pd
+import cv2
 import numpy as np
 
 
-dataset = keras.datasets
-print(dataset.head())
+framewidth = 640
+frameheight = 400
+cap = cv2.VideoCapture(0)
+cap.set(3, framewidth)
+cap.set(4,frameheight)
+
+
+
+
+while True:
+    success, img = cap.read()
+
+    imgBlur = cv2.GaussianBlur(img, (7,7), 1)
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("Result", img)
+    if cv2.waitkey(1) &0xFF == ord('q'):
+        break
