e2c6b10f6687be94e304653babb1c7ebc32e6ba2
dogballlee <zeus.1989@163.com>
2020-07-22 23:02:55 +0800

    nothing new

--- used_car/_sample_plot_test.py
+++ used_car/_sample_plot_test.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -13,15 +12,15 @@ test = path + 'used_car_testB_20200421.csv'
 train_data = pd.read_csv(train, sep= ' ')
 test_data = pd.read_csv(test, sep=' ')
 n_train_data = train_data.select_dtypes(exclude='object').columns
-feature_col = [col for col in n_train_data if col not in['SaleID','name','regDate','creatDate','price','model','brand','regionCode','seller']]
+feature_col = [col for col in n_train_data if col not in['SaleID','name','regDate','creatDate','price','model','brand','regionCode','seller','offerType','v_13','v_14']]
 # print(len(feature_col))
 
 x_train_data = train_data[feature_col]
 x_test_data = test_data[feature_col]
-X_train_scaled = x_train_data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-X_test_scaled = x_test_data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-# X_train_scaled = preprocessing.scale(x_train_data)
-# X_test_scaled = preprocessing.scale(x_test_data)
+# X_train_scaled = x_train_data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+# X_test_scaled = x_test_data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))        #min_max标准化
+X_train_scaled = x_train_data.apply(lambda x: (x - np.mean(x)) / np.std(x))
+X_test_scaled = x_test_data.apply(lambda x: (x - np.mean(x)) / np.std(x))      #z-score标准化
 y_train_data = train_data['price']
 
 # print(x_train_data.shape)   (150000, 21)
@@ -29,7 +28,7 @@ y_train_data = train_data['price']
 # print(x_test_data.shape)    (50000, 21)
 
 for col in feature_col:
-    plt.subplot(3,7,feature_col.index(col)+1)
+    plt.subplot(3,6,feature_col.index(col)+1)
     g = sns.kdeplot(X_train_scaled[col],color='red',shade=True)
     g = sns.kdeplot(X_test_scaled[col], color='blue',ax=g, shade=True)
 plt.show()
--- used_car/main.py
+++ used_car/main.py
@@ -18,14 +18,14 @@ test_data = pd.read_csv(test, sep=' ')
 # print(test_data.shape)
 n_train_data = train_data.select_dtypes(exclude='object').columns
 # print(n_train_data)
-feature_col = [col for col in n_train_data if col not in['SaleID','name','regDate','creatDate','price','model','brand','regionCode','seller']]
+feature_col = [col for col in n_train_data if col not in['SaleID','name','regDate','creatDate','price','model','brand','regionCode','seller','offerType','v_13','v_14']]
 # print(feature_col)
 x_train_data = train_data[feature_col]
-x_train_data = x_train_data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+x_train_data = x_train_data.apply(lambda x: (x - np.mean(x)) / np.std(x))
 y_train_data = train_data['price']
 
 x_test_data = test_data[feature_col]
-x_test_data = x_test_data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+x_test_data = x_test_data.apply(lambda x: (x - np.mean(x)) / np.std(x))
 # print(x_train_data)
 # print(x_test_data)
 #定义了一个用均值填充N/A的函数
@@ -70,57 +70,55 @@ print('val_MAE:',np.mean(scores))
 
 
 
+def build_model_xgb(x_train,y_train):
+    model_xgb = xgb.XGBRegressor(n_estimators=120,learning_rate=.1,gamma=0,subsample=.8,colsample_bytree=.9,max_depth=10)
+    model_xgb.fit(x_train,y_train)
+    return model_xgb
+
+def build_model_lgb(x_train,y_train):
+    estimator = lgb.LGBMRegressor(num_leaves=150,n_estimators=150)
+    param_grid = {'learning_rate': [0.01, 0.05, 0.1, 0.15, 0.2]}
+    gbm = GridSearchCV(estimator,param_grid)
+    gbm.fit(x_train,y_train)
+    return gbm
 
 
-# def build_model_xgb(x_train,y_train):
-#     model_xgb = xgb.XGBRegressor(n_estimators=120,learning_rate=.1,gamma=0,subsample=.8,colsample_bytree=.9,max_depth=10)
-#     model_xgb.fit(x_train,y_train)
-#     return model_xgb
-#
-# def build_model_lgb(x_train,y_train):
-#     estimator = lgb.LGBMRegressor(num_leaves=150,n_estimators=150)
-#     param_grid = {'learning_rate': [0.01, 0.05, 0.1, 0.15, 0.2]}
-#     gbm = GridSearchCV(estimator,param_grid)
-#     gbm.fit(x_train,y_train)
-#     return gbm
-#
-#
-# x_train,x_val,y_train,y_val = train_test_split(x_train_data,y_train_data,test_size=.3)
+x_train,x_val,y_train,y_val = train_test_split(x_train_data,y_train_data,test_size=.3)
 
 
-# #模型训练
-# print('Train lgb...')
-# model_lgb = build_model_lgb(x_train,y_train)
-# val_lgb = model_lgb.predict(x_val)
-# MAE_lgb = mean_absolute_error(y_val,val_lgb)
-# print('MAE of val with lgb:',MAE_lgb)
-#
-# print('Predict lgb...')
-# model_lgb_pre = build_model_lgb(x_train_data,y_train_data)
-# subA_lgb = model_lgb_pre.predict(x_test_data)
-# print('Sta of Predict lgb:')
-#
-# print('Train xgb...')
-# model_xgb = build_model_xgb(x_train,y_train)
-# val_xgb = model_xgb.predict(x_val)
-# MAE_xgb = mean_absolute_error(y_val,val_xgb)
-# print('MAE of val with xgb:',MAE_xgb)
-#
-# print('Predict xgb...')
-# model_xgb_pre = build_model_xgb(x_train_data,y_train_data)
-# subA_xgb = model_xgb_pre.predict(x_test_data)
-# print('Sta of Predict xgb:')
-#
-#
-# #xgb与lgb加权融合
-# val_Weighted = (1-MAE_lgb/(MAE_xgb+MAE_lgb))*val_lgb+(1-MAE_xgb/(MAE_xgb+MAE_lgb))*val_xgb
-# val_Weighted[val_Weighted<0]=10 # 由于我们发现预测的最小值有负数，而真实情况下，price为负是不存在的，由此我们进行对应的后修正
-# print('MAE of val with Weighted ensemble:',mean_absolute_error(y_val,val_Weighted))
-#
-# sub_Weighted = (1-MAE_lgb/(MAE_xgb+MAE_lgb))*subA_lgb+(1-MAE_xgb/(MAE_xgb+MAE_lgb))*subA_xgb
-#
-#
-# sub = pd.DataFrame()
-# sub['SaleID'] = test_data.SaleID
-# sub['price'] = sub_Weighted
-# sub.to_csv('./sub_Weighted.csv',index=False)
+#模型训练
+print('Train lgb...')
+model_lgb = build_model_lgb(x_train,y_train)
+val_lgb = model_lgb.predict(x_val)
+MAE_lgb = mean_absolute_error(y_val,val_lgb)
+print('MAE of val with lgb:',MAE_lgb)
+
+print('Predict lgb...')
+model_lgb_pre = build_model_lgb(x_train_data,y_train_data)
+subA_lgb = model_lgb_pre.predict(x_test_data)
+print('Sta of Predict lgb:')
+
+print('Train xgb...')
+model_xgb = build_model_xgb(x_train,y_train)
+val_xgb = model_xgb.predict(x_val)
+MAE_xgb = mean_absolute_error(y_val,val_xgb)
+print('MAE of val with xgb:',MAE_xgb)
+
+print('Predict xgb...')
+model_xgb_pre = build_model_xgb(x_train_data,y_train_data)
+subA_xgb = model_xgb_pre.predict(x_test_data)
+print('Sta of Predict xgb:')
+
+
+#xgb与lgb加权融合
+val_Weighted = (1-MAE_lgb/(MAE_xgb+MAE_lgb))*val_lgb+(1-MAE_xgb/(MAE_xgb+MAE_lgb))*val_xgb
+val_Weighted[val_Weighted<0]=10 # 由于我们发现预测的最小值有负数，而真实情况下，price为负是不存在的，由此我们进行对应的后修正
+print('MAE of val with Weighted ensemble:',mean_absolute_error(y_val,val_Weighted))
+
+sub_Weighted = (1-MAE_lgb/(MAE_xgb+MAE_lgb))*subA_lgb+(1-MAE_xgb/(MAE_xgb+MAE_lgb))*subA_xgb
+
+
+sub = pd.DataFrame()
+sub['SaleID'] = test_data.SaleID
+sub['price'] = sub_Weighted
+sub.to_csv('./sub_Weighted.csv',index=False)
