48b806a8586960128c9c5ecc95c87de5b2c8b59b
dogballlee <zeus.1989@163.com>
2020-07-27 00:02:45 +0800

    use web cam

--- prjt_cv/use_Keras.py
+++ prjt_cv/use_Keras.py
@@ -1,22 +1,28 @@
 import cv2
 import numpy as np
 
+things_dtct_path = r'C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python37\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml'      #预先训练好的特征池
+
 
 framewidth = 640
 frameheight = 400
 cap = cv2.VideoCapture(0)
 cap.set(3, framewidth)
-cap.set(4,frameheight)
+cap.set(4, frameheight)
 
-
+def discern(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cap = cv2.CascadeClassifier(things_dtct_path)
+    faceRects = cap.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6, minSize=(200, 200))
+    if len(faceRects):
+        for faceRect in faceRects:
+            x, y, w, h = faceRect
+            cv2.rectangle(img, (x, y), (x + h, y + w), (0, 255, 0), 2)  # 框出人脸
+    cv2.imshow("Image", img)
 
 
 while True:
     success, img = cap.read()
-
-    imgBlur = cv2.GaussianBlur(img, (7,7), 1)
-    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow("Result", img)
-    if cv2.waitkey(1) &0xFF == ord('q'):
+    discern(img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
