1180e89a35dffb1809cbff07047cd62ce8b6c081
dogballlee <zeus.1989@163.com>
2021-07-14 23:08:11 +0800

    Add files via upload

--- D2L_pytorch/DRL_DEMO/gym_test.py
+++ D2L_pytorch/DRL_DEMO/gym_test.py
@@ -1,13 +1,13 @@
 import gym
 
 env = gym.make('CartPole-v0')   # 生成cartpole-v0环境
-state = env.reset() # 重置环境，让小车回到起点，并初始化状态
+state = env.reset()     # 重置环境，让小车回到起点，并初始化状态
 
 for t in range(100):
     env.render()    # 弹出窗口，把游戏中发生的显示刀屏幕上
     print(state)
 
-    action = env.action_space.sample()  #抽样生成一个动作，测试用。实际会用策略函数生成动作
+    action = env.action_space.sample()  # 抽样生成一个动作，测试用。实际会用策略函数生成动作
 
     state, reward, done, info = env.step(action)    # 智能体执行动作，更新环境状态，反馈一个奖励
 
