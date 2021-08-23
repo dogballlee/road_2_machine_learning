5ba079d05d8eac70585a0a595627d60f97ea0a95
dogballlee <zeus.1989@163.com>
2021-08-23 23:52:39 +0800

    Add files via upload

--- prototype/retro_test.py
+++ prototype/retro_test.py
@@ -1,13 +1,14 @@
 import retro
 from stable_baselines3 import PPO as PPO
+# from stable_baselines3.common.env_util import make_vec_env, make_atari_env
 
 
 class game:
 
-    def __init__(self, name, time_steps):
+    def __init__(self, name, time_steps, level):
         self.game_name = name
         try:
-            self.env = retro.make(name)
+            self.env = retro.make(name, state=level)
         except NameError:
             print('oops, cant find this name in the retro game list, pls check ur input.')
 
@@ -22,12 +23,12 @@ class game:
         model = PPO.load('PPO_' + self.game_name)
 
         obs = self.env.reset()
-        for i in range(10000):
+        for i in range(1000):
             action, _state = model.predict(obs, deterministic=True)
             obs, reward, done, info = self.env.step(action)
             self.env.render()
 
 
 if __name__ == '__main__':
-    g = game('Airstriker-Genesis', 100000)
+    g = game('Airstriker-Genesis', 8000, 'Level1')
     g()
