cd3ab55a827b833f09fab93ae968b5079eed9ef0
dogballlee <zeus.1989@163.com>
2021-08-26 23:10:31 +0800

    Add files via upload

--- prototype/retro_predict_demo.py
+++ prototype/retro_predict_demo.py
@@ -3,11 +3,11 @@ from retro import Actions
 from stable_baselines3 import PPO as PPO
 # import retro_test
 
-g_name = 'ContraForce-Nes'
-env = retro.make(game=g_name, use_restricted_actions=Actions.DISCRETE)
+g_name = 'Contra-Nes'
+env = retro.make(game=g_name, state='level1', use_restricted_actions=Actions.DISCRETE)
 model = PPO.load('PPO_' + g_name, env=env)
 obs = env.reset()
-for i in range(5000):
+for i in range(2000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
     env.render()
--- prototype/retro_test.py
+++ prototype/retro_test.py
@@ -1,26 +1,39 @@
 import retro
 from retro import Actions
+from gym import ObservationWrapper
 from stable_baselines3 import PPO as PPO
 
 
 # if you want t0 import your own ROM, firsts please make sure they are already include in the retro.data.game_list
 # /,then put it together with ur project
 
+
+class CustomReward(ObservationWrapper):
+    def __init__(self, env=None):
+        super(CustomReward, self).__init__(env)
+        self.observation_space = env.observation_space
+        self.current_position = 0
+        self.current_score = 0
+        self.curr_lives = 2
+
+    def step(self, action):
+        state, _, done, info = self.env.step(action)
+
+
+
 class game:
 
     def __init__(self, name, time_steps, level):
         self.game_name = name
-        try:
-            self.env = retro.make(name, state=level, use_restricted_actions=Actions.DISCRETE)
-        except NameError:
-            print('oops, cant find this name in the retro game list, pls check ur input.')
-
+        self.env = retro.make(name, state=level, use_restricted_actions=Actions.DISCRETE)
         self.total_time_steps = time_steps
 
     def __call__(self):
-        model = PPO('MlpPolicy', self.env, learning_rate=1e-4).learn(self.total_time_steps)
+        policy_kwargs = dict(activation_fn=th.nn.ReLU,
+                             net_arch=[dict(pi=[32, 32], vf=[32, 32])])
+        model = PPO('CnnPolicy', self.env, learning_rate=1e-3, policy_kwargs=policy_kwargs).learn(self.total_time_steps)
         model.save('PPO_' + self.game_name)
-        del model   # the train model is no longer needed any more...
+        del model   # since the model has been trained, its no longer needed any more...
 
         # model = PPO.load('PPO_' + self.game_name)
         #
@@ -32,5 +45,5 @@ class game:
 
 
 if __name__ == '__main__':
-    g = game('ContraForce-Nes', 30000, 'Level1')
+    g = game('Contra-Nes', 300, 'Level1')
     g()
