71dddcd4c1b1c88f7af4fc64b8910ddf795f899b
dogballlee <zeus.1989@163.com>
2021-08-27 23:58:13 +0800

    Add files via upload

--- prototype/retro_test.py
+++ prototype/retro_test.py
@@ -1,6 +1,6 @@
 import retro
+import torch as th
 from retro import Actions
-from gym import ObservationWrapper
 from stable_baselines3 import PPO as PPO
 
 
@@ -8,17 +8,16 @@ from stable_baselines3 import PPO as PPO
 # /,then put it together with ur project
 
 
-class CustomReward(ObservationWrapper):
-    def __init__(self, env=None):
-        super(CustomReward, self).__init__(env)
-        self.observation_space = env.observation_space
-        self.current_position = 0
-        self.current_score = 0
-        self.curr_lives = 2
-
-    def step(self, action):
-        state, _, done, info = self.env.step(action)
-
+# class CustomReward(Wrapper):
+#     def __init__(self, env=None):
+#         super(CustomReward, self).__init__(env)
+#         self.observation_space = env.observation_space
+#         self.current_position = 0
+#         self.current_score = 0
+#         self.curr_lives = 2
+#
+#     def step(self, action):
+#         state, _, done, info = self.env.step(action)
 
 
 class game:
@@ -29,21 +28,15 @@ class game:
         self.total_time_steps = time_steps
 
     def __call__(self):
-        policy_kwargs = dict(activation_fn=th.nn.ReLU,
-                             net_arch=[dict(pi=[32, 32], vf=[32, 32])])
-        model = PPO('CnnPolicy', self.env, learning_rate=1e-3, policy_kwargs=policy_kwargs).learn(self.total_time_steps)
+        policy_kwargs = dict(activation_fn=th.nn.ReLU)
+        model = PPO('CnnPolicy',
+                    self.env,
+                    learning_rate=1e-3,
+                    policy_kwargs=policy_kwargs).learn(self.total_time_steps)
         model.save('PPO_' + self.game_name)
         del model   # since the model has been trained, its no longer needed any more...
 
-        # model = PPO.load('PPO_' + self.game_name)
-        #
-        # obs = self.env.reset()
-        # for i in range(1000):
-        #     action, _state = model.predict(obs, deterministic=True)
-        #     obs, reward, done, info = self.env.step(action)
-        #     self.env.render()
-
 
 if __name__ == '__main__':
-    g = game('Contra-Nes', 300, 'Level1')
+    g = game('Contra-Nes', 3000, 'Level1')
     g()
